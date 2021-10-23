2906fc54abc13cf1a46d6f1354f379d0b86b56ac
Cường Đặng Nguyễn <dangnguyencuong18@gmail.com>
2021-10-23 15:36:05 +0700

    Login

--- ProjectPRN292/Program.cs
+++ ProjectPRN292/Program.cs
@@ -15,7 +15,7 @@ namespace ProjectPRN292
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new BillDetail());
         }
     }
 }
