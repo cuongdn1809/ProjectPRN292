dcb8112288af5820089f14af4dd3099b7d58c72c
DC-COMPUTER <DC-COMPUTER@DESKTOP-7LK7Q66>
2021-10-24 18:26:08 +0700

    KHO

---
+++ ProjectPRN292/WareHouseDetail.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace ProjectPRN292
+{
+    partial class WareHouseDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIDNhap = new System.Windows.Forms.TextBox();
+            this.txtIDCungCap = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.txtIDQuanLy = new System.Windows.Forms.TextBox();
+            this.txtIDChiTiet = new System.Windows.Forms.TextBox();
+            this.SoLuong = new System.Windows.Forms.TextBox();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.txtIDSanPham = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dtpNgayNhap = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(239, 273);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(416, 273);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IDNhapHang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NgayNhap";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IDCungCap";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Note";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "IDQuanLy";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(366, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "IDChiTiet";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(366, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "SoLuong";
+            // 
+            // txtIDNhap
+            // 
+            this.txtIDNhap.Location = new System.Drawing.Point(103, 37);
+            this.txtIDNhap.Name = "txtIDNhap";
+            this.txtIDNhap.Size = new System.Drawing.Size(211, 20);
+            this.txtIDNhap.TabIndex = 9;
+            // 
+            // txtIDCungCap
+            // 
+            this.txtIDCungCap.Location = new System.Drawing.Point(103, 118);
+            this.txtIDCungCap.Name = "txtIDCungCap";
+            this.txtIDCungCap.Size = new System.Drawing.Size(211, 20);
+            this.txtIDCungCap.TabIndex = 11;
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(103, 159);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(211, 20);
+            this.txtNote.TabIndex = 12;
+            // 
+            // txtIDQuanLy
+            // 
+            this.txtIDQuanLy.Location = new System.Drawing.Point(103, 198);
+            this.txtIDQuanLy.Name = "txtIDQuanLy";
+            this.txtIDQuanLy.Size = new System.Drawing.Size(211, 20);
+            this.txtIDQuanLy.TabIndex = 13;
+            // 
+            // txtIDChiTiet
+            // 
+            this.txtIDChiTiet.Location = new System.Drawing.Point(437, 37);
+            this.txtIDChiTiet.Name = "txtIDChiTiet";
+            this.txtIDChiTiet.Size = new System.Drawing.Size(211, 20);
+            this.txtIDChiTiet.TabIndex = 14;
+            // 
+            // SoLuong
+            // 
+            this.SoLuong.Location = new System.Drawing.Point(437, 80);
+            this.SoLuong.Name = "SoLuong";
+            this.SoLuong.Size = new System.Drawing.Size(211, 20);
+            this.SoLuong.TabIndex = 15;
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(437, 118);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(211, 20);
+            this.txtGia.TabIndex = 16;
+            // 
+            // txtIDSanPham
+            // 
+            this.txtIDSanPham.Location = new System.Drawing.Point(437, 159);
+            this.txtIDSanPham.Name = "txtIDSanPham";
+            this.txtIDSanPham.Size = new System.Drawing.Size(211, 20);
+            this.txtIDSanPham.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(366, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Gia";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(366, 162);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "IDSanPham";
+            // 
+            // dtpNgayNhap
+            // 
+            this.dtpNgayNhap.Location = new System.Drawing.Point(103, 75);
+            this.dtpNgayNhap.Name = "dtpNgayNhap";
+            this.dtpNgayNhap.Size = new System.Drawing.Size(211, 20);
+            this.dtpNgayNhap.TabIndex = 21;
+            // 
+            // WareHouseDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 341);
+            this.Controls.Add(this.dtpNgayNhap);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtIDSanPham);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.SoLuong);
+            this.Controls.Add(this.txtIDChiTiet);
+            this.Controls.Add(this.txtIDQuanLy);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.txtIDCungCap);
+            this.Controls.Add(this.txtIDNhap);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Name = "WareHouseDetail";
+            this.Text = "WareHouseDetail";
+            this.Load += new System.EventHandler(this.WareHouseDetail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIDNhap;
+        private System.Windows.Forms.TextBox txtIDCungCap;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.TextBox txtIDQuanLy;
+        private System.Windows.Forms.TextBox txtIDChiTiet;
+        private System.Windows.Forms.TextBox SoLuong;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.TextBox txtIDSanPham;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dtpNgayNhap;
+    }
+}
