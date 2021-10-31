93cf8a772e8daea1ba658067001975c7ca8e9455
Cường Đặng Nguyễn <dangnguyencuong18@gmail.com>
2021-10-31 18:58:19 +0700

    update

--- ProjectPRN292/Entity/KhachHang.cs
+++ ProjectPRN292/Entity/KhachHang.cs
@@ -7,5 +7,26 @@ namespace ProjectPRN292.Entity
 {
     class KhachHang
     {
+        private int khachhangid;
+        private string tenkhachhang;
+        private string diachi;
+        private string sdt;
+
+        public KhachHang()
+        {
+        }
+
+        public KhachHang(int khachhangid, string tenkhachhang, string diachi, string sdt)
+        {
+            Khachhangid = khachhangid;
+            Tenkhachhang = tenkhachhang;
+            Diachi = diachi;
+            Sdt = sdt;
+        }
+
+        public int Khachhangid { get => khachhangid; set => khachhangid = value; }
+        public string Tenkhachhang { get => tenkhachhang; set => tenkhachhang = value; }
+        public string Diachi { get => diachi; set => diachi = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
     }
 }
